c8df6e97554844bb705127567acf36e03e87e6ad
anprice <44292226+anprice@users.noreply.github.com>
2020-05-16 13:11:51 -0400

    Update FutureCalc.py

--- FutureCalc.py
+++ FutureCalc.py
@@ -14,34 +14,38 @@ def calculate_future_value(monthly_investment, yearly_interest, years):
     return future_value
 
 def get_float():
+    min = 0
+    max = 1000
     while True:
         monthly_investment = float(input("Enter monthly investment:\t"))
-        if monthly_investment > 0 and monthly_investment <= 1000:
+        if monthly_investment > min and monthly_investment <= max:
             break
             #return monthly_investment
         else:
-            print("Entry must be greater than 0 and less than or equal to 1000.")
+            print("Entry must be greater than ", min, " and less than or equal to ", max ,".")
             continue
     
-    LOW = 0
-    HIGH = 15
+    low = 0
+    high = 15
     while True:
         yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
-        if yearly_interest_rate > 0 and yearly_interest_rate <= 15:
+        if yearly_interest_rate > low and yearly_interest_rate <= high:
             break
         else:
-            print("Entry must be greater than 0 and less than or equal to 15.")
+            print("Entry must be greater than ", low, " and less than or equal to ", high, ".")
             continue
 
     return monthly_investment, yearly_interest_rate
 
 def get_int():
+    min = 0
+    max = 50
     while True:
         years = int(input("Enter number of years:\t\t"))
-        if years > 0 and years <= 50:
+        if years > min and years <= max:
             break
         else:
-            print("Entry must be greater than 0 and less than or equal to 50.")
+            print("Entry must be greater than ", min, " and less than or equal to ", max, ".")
     
     return years
 
@@ -66,3 +70,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
