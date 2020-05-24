3a810348b0205102bce63408f989076b47337956
anprice <44292226+anprice@users.noreply.github.com>
2020-05-24 03:08:36 -0400

    Update movie_list.py

--- movie_list.py
+++ movie_list.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 
-def display_menu():
+def display_menu():                                             #Function to display the menu list
     print("COMMAND MENU")
     print("list - List all movies")
     print("add  - Add a movie")
     print("del  - Delete a movie")
     print("exit - Exit program")
 
-def list_movies(movie_list):
+def list_movies(movie_list):                                    #Function to list current movies in the list
     i = 1
     for movie in movie_list:
         print(str(i) + "." + movie )
-        i += 1
-        
+        i += 1     
 
-def add_movie(movie_list):
+def add_movie(movie_list):                                      #Function to add movie to list
     movie_list.append(input("Name: "))
     print(movie_list[-1] + " was added.")
-    
 
-def del_movie(movie_list):
+def del_movie(movie_list):                                      #Function to delete movie from list
     num = int(input("Number: "))
     if num < 1 or num  > len(movie_list):
         print("Invalid movie number.\n")
@@ -31,8 +29,10 @@ def del_movie(movie_list):
 def main():
 
     done = False
-    movie_list = ["Monty Python and the Holy Grail", "On the Waterfront", "Cat on a Hot Tin Roof"]
-    display_menu()
+    movie_list = ["Monty Python and the Holy Grail", 
+                  "On the Waterfront", 
+                  "Cat on a Hot Tin Roof"]
+    display_menu()                                              #Calls menu function
     while not done:
         command = str(input("Command: "))
         if command.lower() == "list":
@@ -41,8 +41,10 @@ def main():
             add_movie(movie_list)
         elif command.lower() == "del":
             del_movie(movie_list)
-        else:
+        elif command.lower() == "exit":
             break
+        else:
+            print("Not a valid command.  Please try again.\n")
 
 if __name__ == "__main__":
     main()
