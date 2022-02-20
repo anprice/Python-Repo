dc8c31c0748ebb2f0235cc1fee1a0b1c6b5a7413
anprice <44292226+anprice@users.noreply.github.com>
2022-02-19 22:25:16 -0500

    Update movie_list.py

--- movie_list.py
+++ movie_list.py
@@ -5,6 +5,7 @@ def display_menu():                                                         #Pri
     print("list - List all movies")
     print("add  - Add a movie")
     print("del  - Delete a movie")
+    print("find - Find movies by year")
     print("exit - Exit program")
 
 def list_movies(movie_list):                                                #Displays the list
@@ -14,7 +15,7 @@ def list_movies(movie_list):                                                #Dis
     else:
         i = 1
         for row in movie_list:
-            print(str(i) + ". " + row[0] + " (" + str(row[1]) + ")")
+            print(str(i) + ". " + row[0] + " (" + str(row[1]) + ")" + " @ " + str(row[2]))
             i += 1
         print
         
@@ -35,16 +36,27 @@ def del_movie(movie_list):                                                  #Del
         print("Invalid movie number.\n")
     else:
         movie = movie_list.pop(num-1)
-        print(movie[0] + " (" + str(movie[1]) + ") was deleted.")
+        print(movie[0] + " (" + str(movie[1]) + ")" + " @ " + str(movie[2]) + " was deleted.")
         print()
+
+def find_movie(movie_list):
+
+    year = int(input("Year: "))
+    for movie in movie_list:
+        if movie[1] == year:
+            print(f"{movie[0]} was released in {year}")
+        else:
+            print("No movie found.")
+            break
+    print()
 
 def main():                                                                 #Main function of the program
 
     #Initialize variables and lists
     done = False
-    movie_list = [["Monty Python and the Holy Grail", 1975], 
-                  ["On the Waterfront", 1954], 
-                  ["Cat on a Hot Tin Roof", 1939]]
+    movie_list = [["Monty Python and the Holy Grail", 1975, 9.95], 
+                  ["On the Waterfront", 1954, 5.59], 
+                  ["Cat on a Hot Tin Roof", 1939, 7.95]]
 
     display_menu()
 
@@ -56,6 +68,8 @@ def main():                                                                 #Mai
             add_movie(movie_list)
         elif command.lower() == "del":
             del_movie(movie_list)
+        elif command.lower() == "find":
+            find_movie(movie_list)
         else:
             break
 
